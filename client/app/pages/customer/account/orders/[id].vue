20ef43afd60fe009e2876406d9ec16d997bc7033
Nichiyoubi7 <147227839+Nichiyoubi7@users.noreply.github.com>
2026-04-12 00:56:51 +0800

    Update the id

--- client/app/pages/customer/account/orders/[id].vue
+++ client/app/pages/customer/account/orders/[id].vue
@@ -188,7 +188,7 @@
 
 <script setup lang="ts">
 import { ref, computed, onMounted } from 'vue'
-import GuestLayout from '../../../components/layout/GuestLayout.vue'
+import GuestLayout from '../../../../components/layout/GuestLayout.vue'
 
 const route = useRoute()
 const { get, put } = useApi()
