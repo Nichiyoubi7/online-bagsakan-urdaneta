08ebbc99e49df0208cb9ed23c4996f4b6538609d
Ziehn1 <jayricsondgbasa@gmail.com>
2026-04-20 12:04:58 +0800

    Hide cart from guests

--- client/app/components/nav/NavMainBar.vue
+++ client/app/components/nav/NavMainBar.vue
@@ -76,7 +76,8 @@
         </button>
 
         <!-- Cart -->
-        <NuxtLink to="/customer/cart" class="flex items-center gap-1.5 text-gray-700 hover:text-green-500 transition-colors">
+        <!-- Cart -->
+<NuxtLink v-if="authStore.isLoggedIn" to="/customer/cart" class="flex items-center gap-1.5 text-gray-700 hover:text-green-500 transition-colors">
           <div class="relative">
             <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-1.5 6M17 13l1.5 6M9 19a1 1 0 100 2 1 1 0 000-2zm8 0a1 1 0 100 2 1 1 0 000-2z"/>
